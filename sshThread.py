8bbc7a1b799665ec3154216e2ff12032d8424fd2
Lay-ton <laytonseal@gmail.com>
2019-02-28 18:26:18 -0600

    Cleaned up code, commented, and made paths relative

--- sshThread.py
+++ sshThread.py
@@ -1,83 +1,72 @@
+import os
 import threading
 import subprocess
 import paramiko
 import getpass
-import time
 
 #Gets rid of some annoying output to the terminal from Cryptography being deprecated
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-print threading.current_thread()
-
 #########################################################################
-#								FUNCTIONS								#
+#                                                               FUNCTIONS
 #########################################################################
-def sshThread(machine, server, client, flag, num, run) :
-	client.connect((machine+server), username=username, password=password)
-	std_list["ssh_stdin{0}".format(num)], std_list["ssh_stdout{0}".format(num)], std_list["ssh_stderr{0}".format(num)] = client.exec_command('cd ~/Desktop/CS439/TestOutput/'+machine+'; ./testScript '+str(num)+' '+str(run)+' 1 sghsri')
-	print std_list["ssh_stdout{0}".format(num)].channel.recv_exit_status()
+def sshThread(machine, client, flag, num, run) :
+	client.connect((machine + ".cs.utexas.edu"), username=username, password=password)
+	std_list["ssh_stdin{0}".format(num)], std_list["ssh_stdout{0}".format(num)], std_list["ssh_stderr{0}".format(num)] = client.exec_command('cd ' + script_path + ' ; cd ../TestOutput/' + machine + ' ; ./testScript '+str(num)+' '+str(run)+' 1 sghsri')
+	std_list["ssh_stdout{0}".format(num)].channel.recv_exit_status()
 	std_list["ssh_stdin{0}".format(num)], std_list["ssh_stdout{0}".format(num)], std_list["ssh_stderr{0}".format(num)] = client.exec_command('exit')
 	flag.set()
 
-def check_list(di):
-	for y in di.values():
+def check_list(dic):
+	for y in dic.values():
 		if y.isSet() == False:
 			return False
 	return True
 #########################################################################
 
+# The path to the directory this file is in
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+# Get user login info and the amount of tests to run
 username = raw_input("Username: ")
 password = getpass.getpass();
-runs = input("Number of total runs: ")
-machines = ['hydra', 'risk', 'indus', 'crux']
-server = ".cs.utexas.edu"
-runs = runs / 4
+runs = ((input("Number of total runs: "))/4)
+machines = ['hydra', 'risk', 'indus', 'orion']
 
-# The flag for making sure every thread is done
 lock_list = {}
+ssh_list = {}
+std_list = {}
+thread_list = {}
 for num in xrange(0,len(machines)) :
+	# The flag for making sure every thread is done
 	lock_list["flag{0}".format(num)] = threading.Event()
 
-# Paramiko connections
-ssh_list = {}
-for num in xrange(0,len(machines)) :
+	# Paramiko connections
 	ssh_list["ssh{0}".format(num)] = paramiko.SSHClient()
 	ssh_list["ssh{0}".format(num)].set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-std_list = {}
-for num in xrange(0,len(machines)) :
+	# The ssh input, output, and error for each thread
 	std_list["ssh_stdin{0}".format(num)] = None
 	std_list["ssh_stdout{0}".format(num)] = None
 	std_list["ssh_stderr{0}".format(num)] = None
 
-num = 0
-for item in machines :
-	directory = '/u/' + username +'/Desktop/CS439/TestOutput/' + item
-	subprocess.call(['mkdir', '-p', directory])
-	subprocess.call('cp -r /u/' + username +'/Desktop/CS439/cs439_sp19_lseal_p5/. ' + directory, shell=True)
-	subprocess.call('rm -rf /u/' + username + '/Desktop/CS439/TestOutput/Output'+str(num)+'.txt', shell=True)
-	num += 1
+# Builds the directories for the threads to run in and creates the threads
+for num in xrange(0,len(machines)) :
+	subprocess.call('cd ' + script_path + ' ; cd ../ ; mkdir -p TestOutput/' + machines[num], shell=True)
+	subprocess.call('cp -r ' + script_path + '/. ../TestOutput/' + machines[num], shell=True)
+	subprocess.call('cd ' + script_path + ' ; rm -rf ../TestOutput/Output'+str(num)+'.txt', shell=True)
+	thread_list["thread{0}".format(num)] = threading.Thread(target=sshThread, args=(machines[num], ssh_list["ssh{0}".format(num)], lock_list["flag{0}".format(num)], num, runs,))
 
-thread_list = {}
-
-num = 0
-for item in machines :
-
-	thread_list["thread{0}".format(num)] = threading.Thread(target=sshThread, args=(item, server, ssh_list["ssh{0}".format(num)], lock_list["flag{0}".format(num)], num, runs,))
-	num += 1
-
+# Starts the threads
 for num in xrange(0,len(machines)) :
 	thread_list["thread{0}".format(num)].start()
 
+# While all the threads aren't complete block main from cont
 while not check_list(lock_list) :
 	pass
-	
-for item in machines :
-	directory = '/u/' + username + '/Desktop/CS439/TestOutput/' + item
-	subprocess.call(['rm', '-rf', directory])
 
-
-#NEED TO PUT THIS IN FOR LOOP
+# Deletes the directories and closes the ssh connections
 for num in xrange(0,len(machines)) :
+	subprocess.call('cd ' + script_path + ' ; rm -rf ../TestOutput/' + machines[num], shell=True)
 	ssh_list["ssh{0}".format(num)].close()
