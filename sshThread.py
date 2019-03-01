1f3879207c65ad6067c3c47098c90a0c68936885
Lay-ton <laytonseal@gmail.com>
2019-02-28 21:54:34 -0600

    Included testScript that gets used in the main script, added now takes all user input needed

--- sshThread.py
+++ sshThread.py
@@ -4,16 +4,33 @@ import subprocess
 import paramiko
 import getpass
 
-#Gets rid of some annoying output to the terminal from Cryptography being deprecated
+# Gets rid of some annoying output to the terminal from Cryptography being deprecated
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-#########################################################################
-#                                                               FUNCTIONS
+# The path to the directory this file is in
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+# Get user login info and the amount of tests to run
+username = raw_input("Username: ")
+password = getpass.getpass();
+
+# Takes the testScript arguments a splits them as needed
+test_info = raw_input("#Runs #Tests Names..: ").split()
+test_info[0] = str(int(test_info[0])/4)
+tests_to_run = ""
+for item in test_info[2:] :
+	if item == test_info[-1] :
+		tests_to_run += item
+	else :
+		tests_to_run += (item + " ")
+
+
+# FUNCTIONS
 #########################################################################
 def sshThread(machine, client, flag, num, run) :
 	client.connect((machine + ".cs.utexas.edu"), username=username, password=password)
-	std_list["ssh_stdin{0}".format(num)], std_list["ssh_stdout{0}".format(num)], std_list["ssh_stderr{0}".format(num)] = client.exec_command('cd ' + script_path + ' ; cd ../TestOutput/' + machine + ' ; ./testScript '+str(num)+' '+str(run)+' 1 sghsri')
+	std_list["ssh_stdin{0}".format(num)], std_list["ssh_stdout{0}".format(num)], std_list["ssh_stderr{0}".format(num)] = client.exec_command('cd ' + script_path + ' ; cd ../TestOutput/' + machine + ' ; ./testScript '+str(num)+' '+test_info[0]+' '+test_info[1]+' '+tests_to_run)
 	std_list["ssh_stdout{0}".format(num)].channel.recv_exit_status()
 	std_list["ssh_stdin{0}".format(num)], std_list["ssh_stdout{0}".format(num)], std_list["ssh_stderr{0}".format(num)] = client.exec_command('exit')
 	flag.set()
@@ -25,15 +42,9 @@ def check_list(dic):
 	return True
 #########################################################################
 
-# The path to the directory this file is in
-script_path = os.path.dirname(os.path.realpath(__file__))
-
-# Get user login info and the amount of tests to run
-username = raw_input("Username: ")
-password = getpass.getpass();
-runs = ((input("Number of total runs: "))/4)
 machines = ['hydra', 'risk', 'indus', 'orion']
 
+# Sets up all the dynamic variables
 lock_list = {}
 ssh_list = {}
 std_list = {}
@@ -56,7 +67,7 @@ for num in xrange(0,len(machines)) :
 	subprocess.call('cd ' + script_path + ' ; cd ../ ; mkdir -p TestOutput/' + machines[num], shell=True)
 	subprocess.call('cp -r ' + script_path + '/. ../TestOutput/' + machines[num], shell=True)
 	subprocess.call('cd ' + script_path + ' ; rm -rf ../TestOutput/Output'+str(num)+'.txt', shell=True)
-	thread_list["thread{0}".format(num)] = threading.Thread(target=sshThread, args=(machines[num], ssh_list["ssh{0}".format(num)], lock_list["flag{0}".format(num)], num, runs,))
+	thread_list["thread{0}".format(num)] = threading.Thread(target=sshThread, args=(machines[num], ssh_list["ssh{0}".format(num)], lock_list["flag{0}".format(num)], num, test_info[0],))
 
 # Starts the threads
 for num in xrange(0,len(machines)) :
